6a08ccf73cb7ef4891cbbfa12876abccfaeeaf21
Namit-Nayan <namitnayan32@gmail.com>
2021-06-18 11:29:21 +0530

    Created the Repository(layer between RoomDatabase and ViewModel).

---
+++ app/src/main/java/com/example/takeanote/NoteRepository.kt
@@ -0,0 +1,17 @@
+package com.example.takeanote
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val allNotes: LiveData<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insert(note: Note) {
+        noteDao.insert(note)
+    }
+
+    suspend fun delete(note : Note) {
+        noteDao.delete(note)
+    }
+}
